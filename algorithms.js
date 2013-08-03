9c5b9d234d0584edbc6de8fb3f272f4201240fb6
Guy Morita <guymorita@gmail.com>
2013-08-02 18:57:20 -0700

    working similarity comparision between users. beginning to predict scores for users

--- algorithms.js
+++ algorithms.js
@@ -1,21 +1,7 @@
-var distance = function(users, person1, person2){
-  var commonMovies = {};
-  _.each(users[person1], function(value, key){
-    if (_.contains(users[person2], key)){
-      commonMovies[key]=1;
-    }
-  });
-  if (_.size(commonMovies) === 0){
-    return 0;
-  }
-
-  var sum_of_squares = _.reduce(users[person1], function(memo, value, key){
-    if (users[person2].hasOwnProperty(key)){
-      return memo + Math.pow(users[person1][key]-users[person2][key], 2);
-    }
-  }, 0);
-  return (1/(1+sum_of_squares));
-};
+var redis = require("redis"),
+    client = redis.createClient();
+var async = require('async');
+var config = require('./config.js').config();
 
 // Pearson product-moment correlation coefficient
 
@@ -59,22 +45,102 @@ var pearsonCoefficient = function(users, person1, person2){
   return r;
 };
 
-var jaccardCoefficient = function(userId1, userId2){
+var jaccardCoefficient = function(userId1, userId2, callback){
   var similarity = 0,
   likedCount = 0,
-  dislikedCount = 0;
-  debugger;
-  similarity += _.size(client.sinter(['movie',userId1,'liked'].join(":"),['movie',userId2,'liked'].join(":")));
-  similarity += _.size(client.sinter(['movie',userId1,'disliked'].join(":"),['movie',userId2,'disliked'].join(":")));
-
-  similarity -= _.size(client.sinter(['movie',userId1,'liked'].join(":"),['movie',userId2,'disliked'].join(":")));
-  similarity -= _.size(client.sinter(['movie',userId1,'disliked'].join(":"),['movie',userId2,'liked'].join(":")));
-
-  likedCount += client.scard(['movie',userId1,'liked'].join(":"));
-  dislikedCount += client.scard(['movie',userId1,'disliked'].join(":"));
-
-  console.log(similarity / (likedCount + dislikedCount));
+  dislikedCount = 0,
+  finalJaccard = 0;
+  client.sinter([config.className,userId1,'liked'].join(":"),[config.className,userId2,'liked'].join(":"), function(err, results1){
+    client.sinter([config.className,userId1,'disliked'].join(":"),[config.className,userId2,'disliked'].join(":"), function(err, results2){
+      client.sinter([config.className,userId1,'liked'].join(":"),[config.className,userId2,'disliked'].join(":"), function(err, results3){
+        client.sinter([config.className,userId1,'disliked'].join(":"),[config.className,userId2,'liked'].join(":"), function(err, results4){
+          client.scard([config.className,userId1,'liked'].join(":"), function(err, likedCount){
+            client.scard([config.className,userId1,'disliked'].join(":"), function(err, dislikedCount){
+              similarity += (results1.length+results2.length-results3.length-results4.length);
+              finalJaccardScore = similarity / (likedCount + dislikedCount);
+              callback(finalJaccardScore);
+            });
+          });
+        });
+      });
+    });
+  });
 };
+
+exports.updateSimilarityFor = function(userId){
+  userId = String(userId);
+  var similaritySet, userRatedMovieIds, movieLikedByUsers, movieDislikedByUsers, similarUserIds;
+  similaritySet = [config.className,userId,'similaritySet'].join(":");
+  client.sunion([config.className,userId,'liked'].join(":"),[config.className,userId,'disliked'].join(":"), function(err, userRatedMovieIds){
+    if (userRatedMovieIds.length > 0){
+      similarUserIds = _.map(userRatedMovieIds, function(otherUserRatedId, key){
+        movieLikedByUsers = [config.className, otherUserRatedId, 'liked'].join(":");
+        movieDislikedByUsers = [config.className, otherUserRatedId, 'disliked'].join(":");
+        return [movieLikedByUsers, movieDislikedByUsers];
+      });
+    }
+    similarUserIds = _.flatten(similarUserIds);
+    client.sunion(similarUserIds, function(err, results){
+      _.each(results, function(otherUserId, key){
+        if (userId !== otherUserId){
+          jaccardCoefficient(userId, otherUserId, function(result) {
+            client.zadd(similaritySet, result, otherUserId);
+          });
+        }
+      });
+    });
+  });
+};
+
+exports.predictFor = function(userId, movieId, callback){
+  userId = String(userId);
+  movieId = String(movieId);
+  var finalSimilaritySum = 0.0;
+  var prediction = 0.0;
+  var similaritySet = [config.className,userId,'similaritySet'].join(':');
+  var likedBySet = [config.className, userId, 'liked'].join(':');
+  var dislikedBySet = [config.className, userId, 'disliked'].join(':');
+  similaritySum(similaritySet, likedBySet, function(result){
+    similaritySum(similaritySet, dislikedBySet, function(result2){
+      similaritySum = likedBySet - dislikedBySet;
+      client.scard(likedBySet, function(err, likedByCount){
+        client.scard(dislikedBySet, function(err, dislikedByCount){
+          prediction = similaritySum / parseFloat(likedByCount + dislikedCount);
+          if (isFinite(prediction)){
+            callback(prediction);
+          }
+        });
+      });
+    });
+  });
+};
+
+exports.similaritySum = function(simSet, compSet, cb){
+  var similaritySum = 0.0;
+  client.smembers(compSet, function(err, results){
+    async.each(results,
+      function(score, callback){
+        client.zscore(simSet, id, function(err, score){
+          similaritySum += parseFloat(score);
+          callback();
+        });
+      },
+      function(err){cb(similaritySum);}
+    );
+  });
+};
+
+exports.updateRecommendationsFor = function(userId){
+  userId = String(userId);
+  var nearest_neighbors = config.nearest_neighbors;
+  var ratedSets = [[config.className,userId,'liked'].join(":"),[config.className,userId,'disliked'].join(":")];
+  var tempSet = [config.className, userId, 'tempSet'].join(":");
+  var similaritySet = [config.className, userId, 'similaritySet'].join(":");
+  var recommendedSet = [config.className, userId, 'recommendedSet'].join(":");
+
+  // var mostSimilarUserIds =
+};
+
 exports.topSimilarUsers = function(users, person1, callback, n){
   n = n || 5;
   var name, newObj;
