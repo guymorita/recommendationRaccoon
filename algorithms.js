1d81e2228345590fb6d6e72de45998e13f997d7f
Guy Morita <guymorita@gmail.com>
2013-08-02 23:09:13 -0700

    predictions working but the numbers seem off

--- algorithms.js
+++ algorithms.js
@@ -99,36 +99,39 @@ exports.predictFor = function(userId, movieId, callback){
   movieId = String(movieId);
   var finalSimilaritySum = 0.0;
   var prediction = 0.0;
-  var similaritySet = [config.className,userId,'similaritySet'].join(':');
+  var similaritySet = [config.className, userId, 'similaritySet'].join(':');
   var likedBySet = [config.className, movieId, 'liked'].join(':');
   var dislikedBySet = [config.className, movieId, 'disliked'].join(':');
-  // exports.similaritySum(similaritySet, likedBySet, function(result){
-  //   exports.similaritySum(similaritySet, dislikedBySet, function(result2){
-  //     exports.similaritySum = likedBySet - dislikedBySet;
-  //     client.scard(likedBySet, function(err, likedByCount){
-  //       client.scard(dislikedBySet, function(err, dislikedByCount){
-  //         prediction = exports.similaritySum / parseFloat(likedByCount + dislikedByCount);
-  //         if (isFinite(prediction)){
-  //           callback(prediction);
-  //         }
-  //       });
-  //     });
-  //   });
-  // });
-  callback(1);
+  exports.similaritySum(similaritySet, likedBySet, function(result1){
+    exports.similaritySum(similaritySet, dislikedBySet, function(result2){
+      finalSimilaritySum = result1 - result2;
+      client.scard(likedBySet, function(err, likedByCount){
+        client.scard(dislikedBySet, function(err, dislikedByCount){
+          prediction = finalSimilaritySum / parseFloat(likedByCount + dislikedByCount);
+          if (isFinite(prediction)){
+            console.log('prediction', userId, prediction);
+            callback(prediction);
+          }
+        });
+      });
+    });
+  });
+  // callback(1);
 };
 
 exports.similaritySum = function(simSet, compSet, cb){ // trying to get the score from the similarity set given the userId from the comp set
-  var similaritySum = 0.0;
+  var similarSum = 0.0;
   client.smembers(compSet, function(err, userIds){
     async.each(userIds,
       function(userId, callback){
         client.zscore(simSet, userId, function(err, zScore){
-          similaritySum += parseFloat(zScore);
+          similarSum += parseFloat(zScore);
           callback();
         });
       },
-      function(err){cb(similaritySum);}
+      function(err){
+        cb(similarSum);
+      }
     );
   });
 };
@@ -160,24 +163,24 @@ exports.updateRecommendationsFor = function(userId){
           function(err){
             client.sdiff(tempSet, [config.className,userId,'liked'].join(":"), [config.className,userId,'disliked'].join(":"), function(err, movieIds){
               async.each(movieIds,
-                function(id, callback){
-                  exports.predictFor(userId, id, function(score){
-                    scoreMap.push([score, id]);
+                function(movieId, callback){
+                  exports.predictFor(userId, movieId, function(score){
+                    // console.log('userid', score, movieId);
+                    scoreMap.push([score, movieId]);
                     callback();
                   });
                 },
                 function(err){
                   async.each(scoreMap,
                     function(scorePair, callback){
-                      client.zadd(recommendedSet, scorePair[0], scorePair[1], function(err, results){
-                        console.log('results', results);
+                      client.zadd(recommendedSet, scorePair[0], scorePair[1], function(err){
                         callback();
                       });
                     },
                     function(err){
                       client.del(tempSet, function(err){
                         client.zcard(recommendedSet, function(err, length){
-                          client.zremrangebyrank(recommendedSet, 0, length-config.numOfRecsStore-1);
+                          // client.zremrangebyrank(recommendedSet, 0, length-config.numOfRecsStore-1);
                         });
                       });
                     }
