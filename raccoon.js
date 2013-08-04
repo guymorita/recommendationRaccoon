a823a0656331859947a6475cf2c31ae0e2bcb767
Guy Morita <guymorita@gmail.com>
2013-08-03 23:25:45 -0700

    updated raccoon to use the newer flow through functions Please enter the commit message for your changes. Lines starting

--- raccoon.js
+++ raccoon.js
@@ -1,33 +1,14 @@
 exports.raccoon = function(urlOfDB){
 
-  var models = require('./starter.js').starter(urlOfDB),
-    mongoose = require('mongoose'),
+  var models = require('./sampleContent/starter.js').starter(urlOfDB),
     algo = require('./algorithms.js');
-    config = require('./config.js').config();
     input = require('./input.js').input();
     stat = require('./stat.js').stat();
 
   return {
     models: models,
     input: input,
-    stat: stat,
-
-    recommendationForUser: function(userList, userName, callback){
-      algo.getRecommendations(userList, userName, callback);
-    },
-
-    topSimilarUsers: function(userList, userName, callback){
-      algo.topSimilarUsers(userList, userName, callback);
-    },
-
-    similarity: function(userId1, userId2){
-      algo.jaccardCoefficient(userId1, userId2);
-    }
-
-    // exports.mostLiked = function(){
-
-    // };
-
+    stat: stat
   };
 };
 
