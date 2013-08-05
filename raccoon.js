07ecdeed7d4096a0741e63c753dff044f8c7af63
Guy Morita <guymorita@gmail.com>
2013-08-05 16:27:26 -0700

    added shortcuts for all the inputs and stats

--- raccoon.js
+++ raccoon.js
@@ -1,14 +1,34 @@
 exports.raccoon = function(urlOfDB){
 
-  var models = require('./sampleContent/starter.js').starter(urlOfDB),
-    algo = require('./algorithms.js');
-    input = require('./input.js').input();
-    stat = require('./stat.js').stat();
+  var algo = require('./algorithms.js'),
+  input = require('./input.js').input(),
+  stat = require('./stat.js').stat(),
+  config = require('./config.js').config();
+  if (config.sampleContent){
+    var models = require('./sampleContent/starter.js').starter(urlOfDB);
+  }
 
   return {
     models: models,
     input: input,
-    stat: stat
+    stat: stat,
+    liked: input.liked,
+    disliked: input.disliked,
+    recommendFor: stat.recommendFor,
+    bestRated: stat.bestRated,
+    worstRated: stat.worstRated,
+    mostLiked: stat.mostLiked,
+    mostDisliked: stat.mostDisliked,
+    usersWhoLikedAlsoLiked: stat.usersWhoLikedAlsoLiked,
+    mostSimilarUsers: stat.mostSimilarUsers,
+    leastSimilarUsers: stat.leastSimilarUsers,
+    likedBy: stat.likedBy,
+    likedCount: stat.likedCount,
+    dislikedBy: stat.dislikedBy,
+    dislikedCount: stat.dislikedCount,
+    allLikedFor: stat.allLikedFor,
+    allDislikedFor: stat.allDislikedFor,
+    allWatchedFor: stat.allWatchedFor
   };
 };
 
