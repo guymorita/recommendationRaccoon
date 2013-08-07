7325aac3bea24a3e26e64a26ffc733b67fdbf450
Guy Morita <guymorita@gmail.com>
2013-08-07 16:11:49 -0700

    created file for global settings like redis initiation

--- globalreq.js
+++ globalreq.js
@@ -1,16 +1,20 @@
 var redis = require("redis");
-var config;
+
 module.exports = function() {
+  client = redis.createClient();
   // client = redis.createClient(6379,'127.0.0.1');
-  client = redis.createClient(6379, 'nodejitsudb1343585590.redis.irstack.com');
-  client.auth('nodejitsudb1343585590.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4', function (err) {
-   if (err) { throw err; }
-   // You are now connected to your redis.
-  });
+  // client = redis.createClient(12000, process.env.REDIS_HOST);
+  // client.auth(process.env.REDIS_AUTH, function (err) {
+  //  if (err) { throw err; }
+  //  // You are now connected to your redis.
+  // });
 // client = redis.createClient(6379,'nodejitsudb6625264643.redis.irstack.com');
   // client.auth("nodejitsudb6625264643.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4", function(err, reply) {
   //   if (err) {  console.log("Error: "+err); }
   //   else { console.log("should be connected. Reply: "+reply); }
   // });
-  // client.flushdb();
+  client.flushdb();
+  // client.on('ready', function(){
+
+  // });
 };
