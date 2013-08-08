bb948893ae5eba5a865065d831c2728f90b8926f
Guy Morita <guymorita@gmail.com>
2013-08-07 20:28:44 -0700

    modified the config file to return all the statements and not keep any locally

--- config.js
+++ config.js
@@ -1,15 +1,19 @@
 exports.config = function(){
-  var nearestNeighbors = 5;
-  var className = 'movie';
-  var numOfRecommendationsToStore = 30;
-  var sampleContent = true;
-  var factorLeastSimilarLeastLiked = false;
 
   return {
-    nearestNeighbors: nearestNeighbors,
-    className: className,
-    numOfRecsStore: numOfRecommendationsToStore,
-    sampleContent: sampleContent,
-    factorLeastSimilarLeastLiked: factorLeastSimilarLeastLiked
+    nearestNeighbors: 5,
+    className: 'movie',
+    numOfRecsStore: 30,
+    sampleContent: true,
+    factorLeastSimilarLeastLiked: false,
+    localMongoDbURL: 'mongodb://localhost/users',
+    remoteMongoDbURL: process.env.MONGO_HOSTAUTH,
+    localRedisPort: 6379,
+    localRedisURL: '127.0.0.1',
+    remoteRedisPort: 12000,
+    remoteRedisURL: process.env.REDIS_HOST,
+    remoteRedisAuth: process.env.REDIS_AUTH,
+    flushDBsOnStart: true,
+    localSetup: false
   };
 };
