1e2299583b9255efeaf86bcb0540c0f823030bd5
Guy Morita <guymorita@gmail.com>
2016-12-28 16:37:04 +0700

    Removed testing. Updated versions of async, redis, underscore

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,46 +4,12 @@ module.exports = function(grunt) {
   'use strict';
 
   var files = {
-    src: 'lib/**/*.js',
-    tests: 'test/**/*.js'
+    src: 'lib/**/*.js'
   };
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-
-    jshint: {
-      all: [
-        'Gruntfile.js',
-        files.src,
-        files.tests
-      ]
-    },
-    mochacov: {
-      test: {
-        src: [files.tests]
-      }, // Run with the spec testrunner
-      coverage: {
-        src: [files.tests],
-        options: {
-          coveralls: {
-            serviceName: 'travis-ci',
-            repoToken: process.env.COVERALLS_REPO_TOKEN
-          }
-        }
-      },
-      options: {
-        reporter: 'spec',
-        ignoreLeaks: false,
-        files: [files.tests]
-      }
-    },
-    watch: {
-      tests: {
-        files: _.toArray(files),
-        tasks: ['test']
-      }
-    }
+    pkg: grunt.file.readJSON('package.json')
   });
 
   // On watch events, configure jshint:all to run only on changed file
@@ -51,15 +17,4 @@ module.exports = function(grunt) {
     grunt.config(['jshint', 'all'], filepath);
   });
 
-  // Load third-party modules
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-mocha-cov');
-
-  // Tasks
-  grunt.registerTask('travis', [ 'jshint', 'mochacov:test', 'mochacov:coverage' ]);
-  grunt.registerTask('test', ['jshint:all', 'mochacov:test']);
-
-  // Default task (runs when running `grunt` without arguments)
-  grunt.registerTask('default', ['test']);
 };
